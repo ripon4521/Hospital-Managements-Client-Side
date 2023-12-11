61456706a8d84e69d2961569a0d2b3d9d8f7a47c
ripon4521 <riponakondo4521@gmail.com>
2023-12-12 00:55:23 +0600

    create nested routes and setup daisy ui

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+import {
+
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import router from './Routers/Routers.jsx';
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+<RouterProvider router={router}> </RouterProvider>
   </React.StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,4 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require("daisyui")]}
